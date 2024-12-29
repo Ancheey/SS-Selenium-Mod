a372e00fa04ed991a4b92a50c06fdb738c9d2ab3
Ancheey <Ancheey43@gmail.com>
2024-12-29 15:18:16 +0100

    Dodaj pliki projektów.

---
+++ ModCore.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SeleniteSeaCore;
+using SeleniteSeaEditor.modding;
+
+namespace SS_Selenium_Mod
+{
+    public class ModCore : EditorMod
+    {
+        public override string Name => "Selenium Mod";
+
+        public override string Description => "Adds the selenium package functionality for chrome browser manipulation";
+
+        public override string Version => "0.1";
+
+        public override string Author => "Ancheey";
+
+        public override void OnLoad()
+        {
+            Debug.Log(StatusCode.Success,"Załadowano moda",null);
+        }
+
+        public override void OnRegisterActions()
+        {
+            Debug.Log(StatusCode.Success, "Zarejestrowano moda w edytorze", null);
+        }
+
+        public override void OnRegisterExecutor()
+        {
+            Debug.Log(StatusCode.Success, "Zarejestrowano moda w egzekutorze", null);
+        }
+    }
+}
